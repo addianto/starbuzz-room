1b314a6df41a1f225f5778faaed410f84963eda8
Daya Adianto <dayaadianto@cs.ui.ac.id>
2018-10-17 21:42:40 +0700

    Migrate code & dependencies to use Android Jetpack

--- app/src/main/java/com/hfad/starbuzzroom/TopLevelActivity.java
+++ app/src/main/java/com/hfad/starbuzzroom/TopLevelActivity.java
@@ -1,7 +1,8 @@
 package com.hfad.starbuzzroom;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class TopLevelActivity extends AppCompatActivity {
 
