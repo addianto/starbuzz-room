8e8c50cbc3fec288b3fc4f8ad4a6fceb49e0f6e3
Daya Adianto <dayaadianto@cs.ui.ac.id>
2018-10-17 20:25:19 +0700

    Add basic template generated by Android Studio

---
+++ app/src/main/java/com/hfad/starbuzzroom/TopLevelActivity.java
@@ -0,0 +1,13 @@
+package com.hfad.starbuzzroom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TopLevelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_level);
+    }
+}
