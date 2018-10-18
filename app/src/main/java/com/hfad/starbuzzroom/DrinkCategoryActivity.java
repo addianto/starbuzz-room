2ea472c09fa19808630ce847e4145026152377c1
Daya Adianto <dayaadianto@cs.ui.ac.id>
2018-10-18 10:04:54 +0700

    Add drink category activity

---
+++ app/src/main/java/com/hfad/starbuzzroom/DrinkCategoryActivity.java
@@ -0,0 +1,13 @@
+package com.hfad.starbuzzroom;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrinkCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink_category);
+    }
+}
--- app/src/main/java/com/hfad/starbuzzroom/TopLevelActivity.java
+++ app/src/main/java/com/hfad/starbuzzroom/TopLevelActivity.java
@@ -1,6 +1,10 @@
 package com.hfad.starbuzzroom;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +14,14 @@ public class TopLevelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+
+        ListView listView = findViewById(R.id.list_options);
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            if (position == 0) {
+                Intent intent = new Intent(TopLevelActivity.this,
+                        DrinkCategoryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
